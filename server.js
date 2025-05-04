88fd81325c63c4540abf4c23748846cc80e0d8c2
Matheus Rodrigues de Lima <mhs.djc@gmail.com>
2025-05-04 12:13:34 -0300

    add nome e idade

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/hello", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+app.get('/ap', (req, res) => {
+  const nome = req.query.nome;
+  const idade = req.query.idade;
+
+  res.send(`Olá, ${nome}. Sua idade é ${idade}.`);
+});
+
+
 // Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
